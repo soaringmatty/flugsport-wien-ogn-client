eea4736d1573d6c98242781bb590635c04ecbd04
Matthias Buschbeck <matthias.buschbeck@outlook.com>
2025-06-16 22:53:50 +0200

    Adapt indent size in store file

--- src/app/store/ogn.store.ts
+++ src/app/store/ogn.store.ts
@@ -17,261 +17,261 @@ import { AircraftType } from "../models/aircraft-type";
 
 
 type OgnState = {
-    flights: Flight[];
-    selectedAircraft: string | null;
-    selectedAircraftFlightData: Flight | null;
-    flightHistory: HistoryEntry[];
-    settings: MapSettings;
-    departureList: DepartureListItem[];
-    searchText: string;
-    searchResult: SearchResultItem[];
-    mapTarget: MapTarget | null;
-    historicFlightTarget: { flarmId: string, start?: string, end?: string } | null;
+  flights: Flight[];
+  selectedAircraft: string | null;
+  selectedAircraftFlightData: Flight | null;
+  flightHistory: HistoryEntry[];
+  settings: MapSettings;
+  departureList: DepartureListItem[];
+  searchText: string;
+  searchResult: SearchResultItem[];
+  mapTarget: MapTarget | null;
+  historicFlightTarget: { flarmId: string, start?: string, end?: string } | null;
 };
 
 const initialState: OgnState = {
-    flights: [],
-    selectedAircraft: null,
-    selectedAircraftFlightData: null,
-    flightHistory: [],
-    settings: defaultSettings,
-    departureList: [],
-    searchText: '',
-    searchResult: [],
-    mapTarget: null,
-    historicFlightTarget: null
+  flights: [],
+  selectedAircraft: null,
+  selectedAircraftFlightData: null,
+  flightHistory: [],
+  settings: defaultSettings,
+  departureList: [],
+  searchText: '',
+  searchResult: [],
+  mapTarget: null,
+  historicFlightTarget: null
 };
 
 export const OgnStore = signalStore(
-    { providedIn: 'root' },
-    withState(initialState),
-    withMethods(state => {
-        const apiService = inject(ApiService);
-        const settingsService = inject(SettingsService);
-        const notificationService = inject(NotificationService);
+  { providedIn: 'root' },
+  withState(initialState),
+  withMethods(state => {
+    const apiService = inject(ApiService);
+    const settingsService = inject(SettingsService);
+    const notificationService = inject(NotificationService);
 
-        return {
-            loadFlights: async (
-                maxLat: number,
-                minLat: number,
-                maxLng: number,
-                minLng: number,
-                selectedFlarmId?: string,
-                glidersOnly?: boolean,
-                clubGlidersOnly?: boolean,
-            ) => {
-                try {
-                    const flights = await apiService.getFlights(
-                        maxLat, minLat, maxLng, minLng,
-                        selectedFlarmId, glidersOnly, clubGlidersOnly
-                    );
+    return {
+      loadFlights: async (
+        maxLat: number,
+        minLat: number,
+        maxLng: number,
+        minLng: number,
+        selectedFlarmId?: string,
+        glidersOnly?: boolean,
+        clubGlidersOnly?: boolean,
+      ) => {
+        try {
+          const flights = await apiService.getFlights(
+            maxLat, minLat, maxLng, minLng,
+            selectedFlarmId, glidersOnly, clubGlidersOnly
+          );
 
-                    const selected = state.selectedAircraftFlightData();
-                    const updatedSeletedFlight = flights.find(f => f.flarmId === selectedFlarmId);
-                    const hasChanged = updatedSeletedFlight && updatedSeletedFlight.timestamp !== selected?.timestamp;
+          const selected = state.selectedAircraftFlightData();
+          const updatedSeletedFlight = flights.find(f => f.flarmId === selectedFlarmId);
+          const hasChanged = updatedSeletedFlight && updatedSeletedFlight.timestamp !== selected?.timestamp;
 
-                    const flightHistory = state.flightHistory();
-                    let updatedHistory = [...flightHistory];
+          const flightHistory = state.flightHistory();
+          let updatedHistory = [...flightHistory];
 
-                    if (updatedSeletedFlight && hasChanged) {
-                        updatedHistory.push({
-                            timestamp: updatedSeletedFlight.timestamp,
-                            altitude: updatedSeletedFlight.heightMSL,
-                            latitude: updatedSeletedFlight.latitude,
-                            longitude: updatedSeletedFlight.longitude,
-                            speed: updatedSeletedFlight.speed,
-                            verticalSpeed: updatedSeletedFlight.vario,
-                        });
-                    }
+          if (updatedSeletedFlight && hasChanged) {
+            updatedHistory.push({
+              timestamp: updatedSeletedFlight.timestamp,
+              altitude: updatedSeletedFlight.heightMSL,
+              latitude: updatedSeletedFlight.latitude,
+              longitude: updatedSeletedFlight.longitude,
+              speed: updatedSeletedFlight.speed,
+              verticalSpeed: updatedSeletedFlight.vario,
+            });
+          }
 
-                    patchState(state, current => ({
-                        ...current,
-                        flights,
-                    }));
-                    // Do not update flight path and flight data if historicFlightTarget is set
-                    if (hasChanged && !state.historicFlightTarget()) {
-                        patchState(state, current => ({
-                            ...current,
-                            flightHistory: updatedSeletedFlight ? updatedHistory : current.flightHistory,
-                            selectedAircraftFlightData: updatedSeletedFlight || current.selectedAircraftFlightData,
-                        }));
-                    };
-                } catch (error) {
-                    console.log(error);
-                    notificationService.notify({
-                        message: messages.defaultNetworkError,
-                        type: NotificationType.Error
-                    });
-                }
-            },
+          patchState(state, current => ({
+            ...current,
+            flights,
+          }));
+          // Do not update flight path and flight data if historicFlightTarget is set
+          if (hasChanged && !state.historicFlightTarget()) {
+            patchState(state, current => ({
+              ...current,
+              flightHistory: updatedSeletedFlight ? updatedHistory : current.flightHistory,
+              selectedAircraftFlightData: updatedSeletedFlight || current.selectedAircraftFlightData,
+            }));
+          };
+        } catch (error) {
+          console.log(error);
+          notificationService.notify({
+            message: messages.defaultNetworkError,
+            type: NotificationType.Error
+          });
+        }
+      },
 
-            loadFlightHistory: async (flarmId: string, startTimestamp?: string, endTimestamp?: string) => {
-                try {
-                    const history = await apiService.getFlightHistory(flarmId, startTimestamp, endTimestamp);
-                    patchState(state, current => ({
-                        ...current,
-                        flightHistory: history
-                    }));
-                } catch (error) {
-                    notificationService.notify({
-                        message: messages.defaultNetworkError,
-                        type: NotificationType.Error
-                    });
-                }
-            },
+      loadFlightHistory: async (flarmId: string, startTimestamp?: string, endTimestamp?: string) => {
+        try {
+          const history = await apiService.getFlightHistory(flarmId, startTimestamp, endTimestamp);
+          patchState(state, current => ({
+            ...current,
+            flightHistory: history
+          }));
+        } catch (error) {
+          notificationService.notify({
+            message: messages.defaultNetworkError,
+            type: NotificationType.Error
+          });
+        }
+      },
 
-            selectAircraft: (flarmId: string | null) => {
-                if (flarmId) {
-                    patchState(state, current => ({
-                        ...current,
-                        selectedAircraft: flarmId,
-                    }));
-                }
-                else {
-                    patchState(state, current => ({
-                        ...current,
-                        selectedAircraft: initialState.selectedAircraft,
-                        selectedAircraftFlightData: state.historicFlightTarget() ? state.selectedAircraftFlightData() : initialState.selectedAircraftFlightData,
-                        flightHistory: initialState.flightHistory
-                    }));
-                }
-            },
+      selectAircraft: (flarmId: string | null) => {
+        if (flarmId) {
+          patchState(state, current => ({
+            ...current,
+            selectedAircraft: flarmId,
+          }));
+        }
+        else {
+          patchState(state, current => ({
+            ...current,
+            selectedAircraft: initialState.selectedAircraft,
+            selectedAircraftFlightData: state.historicFlightTarget() ? state.selectedAircraftFlightData() : initialState.selectedAircraftFlightData,
+            flightHistory: initialState.flightHistory
+          }));
+        }
+      },
 
-            setSelectedAircraftFlightData: (flightData: Flight | null) => {
-                if (flightData) {
-                    patchState(state, current => ({
-                        ...current,
-                        selectedAircraftFlightData: flightData,
-                    }));
-                }
-            },
+      setSelectedAircraftFlightData: (flightData: Flight | null) => {
+        if (flightData) {
+          patchState(state, current => ({
+            ...current,
+            selectedAircraftFlightData: flightData,
+          }));
+        }
+      },
 
-            saveSettings: (settings: MapSettings) => {
-                settingsService.saveSettings(settings);
-                patchState(state, current => ({
-                    ...current,
-                    settings
-                }));
-            },
+      saveSettings: (settings: MapSettings) => {
+        settingsService.saveSettings(settings);
+        patchState(state, current => ({
+          ...current,
+          settings
+        }));
+      },
 
-            loadSettings: () => {
-                const loaded = settingsService.loadSettings();
-                patchState(state, current => ({
-                    ...current,
-                    settings: loaded
-                }));
-            },
+      loadSettings: () => {
+        const loaded = settingsService.loadSettings();
+        patchState(state, current => ({
+          ...current,
+          settings: loaded
+        }));
+      },
 
-            loadDepartureList: async (knownGlidersOnly: boolean) => {
-                try {
-                    const departureList = await apiService.getDepartureList(knownGlidersOnly, state.settings().useNewDepartureList);
-                    patchState(state, current => ({
-                        ...current,
-                        departureList
-                    }));
-                } catch (error) {
-                    notificationService.notify({
-                        message: messages.defaultNetworkError,
-                        type: NotificationType.Error
-                    });
-                }
-            },
+      loadDepartureList: async (knownGlidersOnly: boolean) => {
+        try {
+          const departureList = await apiService.getDepartureList(knownGlidersOnly, state.settings().useNewDepartureList);
+          patchState(state, current => ({
+            ...current,
+            departureList
+          }));
+        } catch (error) {
+          notificationService.notify({
+            message: messages.defaultNetworkError,
+            type: NotificationType.Error
+          });
+        }
+      },
 
-            searchAircraft: async (searchText: string) => {
-                try {
-                    const searchResult = await apiService.searchAircraft(searchText);
-                    patchState(state, current => ({
-                        ...current,
-                        searchText,
-                        searchResult
-                    }));
-                } catch (error) {
-                    notificationService.notify({
-                        message: messages.defaultNetworkError,
-                        type: NotificationType.Error
-                    });
-                }
-            },
+      searchAircraft: async (searchText: string) => {
+        try {
+          const searchResult = await apiService.searchAircraft(searchText);
+          patchState(state, current => ({
+            ...current,
+            searchText,
+            searchResult
+          }));
+        } catch (error) {
+          notificationService.notify({
+            message: messages.defaultNetworkError,
+            type: NotificationType.Error
+          });
+        }
+      },
 
-            clearSearchResult: () => {
-                patchState(state, current => ({
-                    ...current,
-                    searchText: initialState.searchText,
-                    searchResult: initialState.searchResult
-                }));
-            },
+      clearSearchResult: () => {
+        patchState(state, current => ({
+          ...current,
+          searchText: initialState.searchText,
+          searchResult: initialState.searchResult
+        }));
+      },
 
-            setMapTarget(flarmId: string, lat: number, lng: number, flightStatus: FlightStatus): void {
-                patchState(state, current => ({
-                    ...current,
-                    mapTarget: { flarmId, lat, lng, flightStatus }
-                }));
-            },
+      setMapTarget(flarmId: string, lat: number, lng: number, flightStatus: FlightStatus): void {
+        patchState(state, current => ({
+          ...current,
+          mapTarget: { flarmId, lat, lng, flightStatus }
+        }));
+      },
 
-            loadAndSetMapTarget: async (flarmId: string) => {
-                try {
-                    const searchResult = await apiService.searchAircraft(flarmId);
-                    if (searchResult && searchResult.length > 0) {
-                        var aircraft = searchResult[0];
-                        patchState(state, current => ({
-                            ...current,
-                            mapTarget: {
-                                flarmId: aircraft.flarmId,
-                                lat: aircraft.latitude,
-                                lng: aircraft.longitude,
-                                flightStatus: aircraft.flightStatus
-                            }
-                        }));
-                    }
-                } catch (error) {
-                    notificationService.notify({
-                        message: messages.defaultNetworkError,
-                        type: NotificationType.Error
-                    });
-                }
-            },
+      loadAndSetMapTarget: async (flarmId: string) => {
+        try {
+          const searchResult = await apiService.searchAircraft(flarmId);
+          if (searchResult && searchResult.length > 0) {
+            var aircraft = searchResult[0];
+            patchState(state, current => ({
+              ...current,
+              mapTarget: {
+                flarmId: aircraft.flarmId,
+                lat: aircraft.latitude,
+                lng: aircraft.longitude,
+                flightStatus: aircraft.flightStatus
+              }
+            }));
+          }
+        } catch (error) {
+          notificationService.notify({
+            message: messages.defaultNetworkError,
+            type: NotificationType.Error
+          });
+        }
+      },
 
-            clearMapTarget(): void {
-                patchState(state, current => ({
-                    ...current,
-                    mapTarget: initialState.mapTarget
-                }));
-            },
+      clearMapTarget(): void {
+        patchState(state, current => ({
+          ...current,
+          mapTarget: initialState.mapTarget
+        }));
+      },
 
-            setHistoricFlightTarget(departureListItem: DepartureListItem): void {
-                patchState(state, current => ({
-                    ...current,
-                    historicFlightTarget: { 
-                      flarmId: departureListItem.flarmId,
-                      start: departureListItem.departureTimestamp,
-                      end: departureListItem.landingTimestamp
-                    },
-                    selectedAircraftFlightData: {
-                      flarmId: departureListItem.flarmId,
-                      displayName: departureListItem.registrationShort,
-                      registration: departureListItem.registration,
-                      // Irrelevant placeholder values -> its not displayed
-                      type: GliderType.foreign,
-                      aircraftType: AircraftType.unknown,
-                      model: '',
-                      latitude: 0,
-                      longitude: 0,
-                      heightMSL: 0,
-                      timestamp: '',
-                      speed: 0,
-                      vario: 0,
-                      varioAverage: 0
-                    }
-                }));
-            },
+      setHistoricFlightTarget(departureListItem: DepartureListItem): void {
+        patchState(state, current => ({
+          ...current,
+          historicFlightTarget: {
+            flarmId: departureListItem.flarmId,
+            start: departureListItem.departureTimestamp,
+            end: departureListItem.landingTimestamp
+          },
+          selectedAircraftFlightData: {
+            flarmId: departureListItem.flarmId,
+            displayName: departureListItem.registrationShort,
+            registration: departureListItem.registration,
+            // Irrelevant placeholder values -> its not displayed
+            type: GliderType.foreign,
+            aircraftType: AircraftType.unknown,
+            model: '',
+            latitude: 0,
+            longitude: 0,
+            heightMSL: 0,
+            timestamp: '',
+            speed: 0,
+            vario: 0,
+            varioAverage: 0
+          }
+        }));
+      },
 
-            clearHistoricFlightTarget(): void {
-                patchState(state, current => ({
-                    ...current,
-                    historicFlightTarget: initialState.historicFlightTarget,
-                    selectedAircraftFlightData: initialState.selectedAircraftFlightData
-                }));
-            },
-        };
-    })
+      clearHistoricFlightTarget(): void {
+        patchState(state, current => ({
+          ...current,
+          historicFlightTarget: initialState.historicFlightTarget,
+          selectedAircraftFlightData: initialState.selectedAircraftFlightData
+        }));
+      },
+    };
+  })
 );
